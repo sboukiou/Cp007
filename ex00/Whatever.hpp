84935006d32226d2d1cae2594205d3188071bc60
sboukiou <sboukiou@student.1337.ma>
2026-01-09 14:28:14 +0100

    Ex00 added

---
+++ ex00/Whatever.hpp
@@ -0,0 +1,30 @@
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+template <typename T>
+
+void	swap(T &A, T &B) {
+	T temp;
+	temp = A;
+	A = B;
+	B = temp;
+}
+
+template <typename T>
+T	min(T &A, T &B) {
+	if (A > B)
+		return (B);
+	if (B > A)
+		return (A);
+	return (B);
+}
+
+template <typename T>
+T	max(T &A, T &B) {
+	if (A > B)
+		return (A);
+	if (B > A)
+		return (A);
+	return (B);
+}
+#endif /* WHATEVER_HPP */
